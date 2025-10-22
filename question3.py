19678323434b3105e63f57169b73903c5239f27f
Lanre Akinbo <lanre.akinbo@gmail.com>
2025-10-22 22:45:00 +0100

    Updating code

--- question3.py
+++ question3.py
@@ -23,28 +23,29 @@ def calculate_average(scores):
 
 def get_letter_grade(average):
     if average < 60:
-        return "Grade: F"
+        return "Letter Grade: F"
     elif average <=69:
-        return "Grade: D"
+        return "Letter Grade: D"
     elif average <=79:
-        return "Grade: C"
+        return "Letter Grade: C"
     elif average <=89:
-        return "Grade: B"
+        return "Letter Grade: B"
     else:
-        return "Grade A"
+        return "Letter Grade A"
 
 reported_scores = []
 counter = 1
 try:
-    scores_count = int(input("How many scores do you want to report?"))
+    scores_count = int(input("How many test scores do you want to enter? "))
     while counter <= scores_count:
-        new_score = float(input(f"Enter Score {counter}: "))
+        new_score = float(input(f"Enter score {counter}: "))
         reported_scores.append(new_score)
         counter += 1
     average_score = calculate_average(reported_scores)
 
-    print("******************************************")
-    print(f"Average of {scores_count} reported scores: {average_score:.2f}")
+    print("\n===GRADE REPORT ===")
+    print(f"Test Scores: {reported_scores}")
+    print(f"Average Score: {average_score:.1f}")
     print(get_letter_grade(average_score))
 
 except ValueError:
